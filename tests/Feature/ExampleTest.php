7e38939916aef269d66f359e468123f89d707b15
Matija Belec <matijabelec1@gmail.com>
2022-04-03 03:06:36 +0200

    Add PhpUnit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testTrueIsAlwaysTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
